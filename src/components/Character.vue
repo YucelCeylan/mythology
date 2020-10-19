23139fddff0aa2af802310dacbd780f756387f9e
DESKTOP-JGVV1NQ\Yucel <yucelceylan89@gmail.com>
2020-10-20 00:25:46 +0300

    Localization ve characterile generic yapı

---
+++ src/components/Character.vue
@@ -0,0 +1,50 @@
+<template>
+    <span>
+      <el-popover
+          ref="popover"
+          placement="top-start"
+          title="{{ $t('characterTitle') }}"
+          width="600"
+          trigger="hover">
+          <el-row>
+            <el-col :span="16">
+              <div style="padding-right:10px; text-align: justify; word-break: normal;">
+                <span>{{ $t('shortDefinition') }}</span>
+              </div>
+            </el-col>
+            <el-col :span="8">
+              <el-image style="width: 190px; height: 220px" :src="image" :fit="fit"></el-image>
+            </el-col>
+          </el-row>
+      </el-popover>
+      <el-link v-popover:popover type="primary">{{ $t("linkName") }}</el-link>
+    </span>
+</template>
+
+<script>
+  import Vue from 'vue'
+  import VueI18n from 'vue-i18n'
+  Vue.use(VueI18n)
+
+  export default {
+    name: 'Character',
+    props: {
+      image: String,
+      characterData: Object
+    },
+    beforeMount() {
+       var i18n = new VueI18n({locale, characterData})
+    },
+    data() {
+      return {
+        locale: process.env.VUE_APP_I18N_LOCALE || 'en'
+      };
+    },
+    methods: {
+      
+    }
+  }
+</script>
+
+<style>
+</style>
--- src/names/Gaia.vue
+++ src/names/Gaia.vue
@@ -3,15 +3,13 @@
       <el-popover
           ref="popover"
           placement="top-start"
-          title="Gaia / Gaea (Roma: Terra)"
+          title= "{{$t('shortDefinition')}}"
           width="600"
           trigger="hover">
           <el-row>
             <el-col :span="16">
               <div style="padding-right:10px; text-align: justify; word-break: normal;">
-                <span>Yeryüzünü, doğayı ve toprağı simgeleyen birinci nesil tanrıdır. Doğa ana ve tüm tanrıların anası olarak
-                  görülmüştür. Khaos'un getirdiği düzensizlikten doğarak evrene düzeni getirmiştir. Daha sonra Uranos'u, Dağları 
-                  ve Denizi yaratmıştır. Uranos(Gök) ile birleşerek Titanlar'ı, Tepegözler'i ve Hekatonkheir'leri doğurmuştur.</span>
+                <span>{{ $t('shortDefinition') }}</span>
               </div>
             </el-col>
             <el-col :span="8">
@@ -31,6 +29,19 @@
     data() {
       return {
         gaiaProfile: require('@/assets/gaia_profile.png'),
+        characterData: {
+          tr: {
+            name: "Gaia / Gaea (Roma: Terra)",
+            link: "Gaia",
+            shortDefinition: "Yeryüzünü, doğayı ve toprağı simgeleyen birinci nesil tanrıdır. Doğa ana ve tüm tanrıların anası olarak görülmüştür. Kaos'un getirdiği düzensizlikten doğarak evrene düzeni getirmiştir. Daha sonra Uranos'u, Dağları ve Denizi yaratmıştır. Uranos (Gök) ile birleşerek Titanlar'ı, Tepegözler'i ve Hekatonkheir'leri doğurmuştur."
+          },
+
+          en: {
+            name: "Gaia / Gaea (Roman: Terra)",
+            link: "Gaia",
+            shortDefinition: "She is the first generation god who symbolizes the earth and nature. She was seen as mother nature and the mother of all gods. She brought order to the universe, arising from the disorder brought about by chaos. Shee later created Uranos, Mountains and Sea. Merged with Uranos (Sky) to give birth to the Titans, Cyclopes and Hekatonkheirs."
+          }
+        }
       };
     },
     methods: {
